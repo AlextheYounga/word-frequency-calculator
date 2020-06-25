a97eeffcb73a91c0a3c58ec128e3195d61854b1c
Alex Younger <alex@izoox.com>
2020-06-25 12:49:08 -0600

    Graph occurance of word in text function

--- functions.py
+++ functions.py
@@ -1,9 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from os import path
+from pathlib import Path
+from definitions import ROOT_DIR
 import string
 from collections import Counter
 import statistics
+import json
+import time
 
 
 def syllable_average(lexicon):
+    #Will calculate the average syllable count of the entire lexicon
     syllable_lst = []
     for word in lexicon:
         word = word.lower()
@@ -34,3 +42,64 @@ def count_frequency(lexicon):
         counts = dict((k[0], k[1:][0]) for k in counts) # Converting Python's most_common results into dictionary
 
     return counts
+
+
+def scan_files(files, folder, imports_folder):
+    lexicon = []
+    full_text = ""
+    for f in files:
+        if (Path(f).suffix == '.txt'):
+            with open(path.join(imports_folder, folder, f), 'r') as file:
+                txt = file.read().replace('\n', '')  # converting txt file into string
+                full_text += txt # appending to full_text string
+                lexicon.extend(txt.split())  # converting string into list
+
+        else:
+            print("Error: Incorrect file type provided")
+            break
+
+    frequency = count_frequency(lexicon)
+    syllables = syllable_average(lexicon)
+    most_common_word = list(frequency.keys())[0]
+
+    print(json.dumps(frequency, indent=1))
+    print("\nAverage Syllables: {}".format(syllables))
+
+    return
+
+
+def graph_incidence(files, folder, imports_folder):
+    # Will graph the occurrance of the most common word throughout the text
+    lexicon = []
+    full_text = ""
+    for f in files:
+        if (Path(f).suffix == '.txt'):
+            with open(path.join(imports_folder, folder, f), 'r') as file:
+                txt = file.read().replace('\n', '')  # converting txt file into string
+                full_text += txt # appending to full_text string
+                lexicon.extend(txt.split())  # converting string into list
+
+        else:
+            print("Error: Incorrect file type provided")
+            break
+
+    frequency = count_frequency(lexicon)    
+    most_common_word = list(frequency.keys())[0]
+
+    sentences = txt.split('.')  # dividing txt by sentence
+    data = {}
+    for i, sentence in enumerate(sentences):
+        count = sentence.count(most_common_word)
+        data[i] = count
+
+    #graphing the data
+    x = list(data.keys())
+    plt.plot(x, list(data.values()), label='Occurance')  # Plot some data on the (implicit) axes.
+    plt.xlabel('x Time')
+    plt.ylabel('y Occurance')
+    plt.title("Word: \"{}\" Incidence".format(most_common_word))
+    plt.legend()    
+    # plt.show()
+    plt.draw()
+    plt.pause(0.001)
+    input("Press [enter] to continue.")
--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 from os import listdir, scandir, path
 import multiprocessing as mp
 from definitions import *
-from functions import *
+from functions import graph_incidence
 
 
 # Begin scanning imports folder
@@ -10,5 +10,5 @@ for folder in folders:
     files = listdir(folder)
     if __name__ == '__main__':
         pool = mp.Pool(mp.cpu_count()) # Init multiprocessing.Pool()
-        process = pool.apply_async(scan_files(files, folder, IMPORTS)) # adding process to pool
+        process = pool.apply_async(graph_incidence(files, folder, IMPORTS)) # adding process to pool
         pool.close() #closing pool
