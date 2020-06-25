40398fa9d2b220778a712ced9ca898f4d9650f51
Alex Younger <alex@izoox.com>
2020-06-25 01:14:49 -0600

    Setting up multiprocessing

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 from os import listdir, scandir, path
 from pathlib import Path
-from multiprocessing import Pool
+import multiprocessing as mp
 import json
 from definitions import ROOT_DIR
 from functions import *
@@ -8,12 +8,9 @@ from functions import *
 
 imports_folder = "{}/imports/".format(ROOT_DIR)
 
-# Begin scanning imports folder
-folders = scandir(imports_folder)
-for folder in folders:
-    files = listdir(folder)
+def scan_files(files):
     lexicon = []
-
+    results = {}
     for f in files:
         if (Path(f).suffix == '.txt'):
             with open(path.join(imports_folder, folder, f), 'r') as file:
@@ -23,9 +20,20 @@ for folder in folders:
         else:
             print("Error: Incorrect file type provided")
             break
-
+    
     frequency = count_frequency(lexicon)
     syllables = syllable_average(lexicon)
 
     print(json.dumps(frequency, indent=1))
     print("\nAverage Syllables: {}".format(syllables))
+
+    return
+
+# Begin scanning imports folder
+folders = scandir(imports_folder)
+for folder in folders:
+    files = listdir(folder)
+    if __name__ == '__main__':
+        pool = mp.Pool(mp.cpu_count()) # Init multiprocessing.Pool()
+        process = pool.apply_async(scan_files(files)) # adding process to pool
+        pool.close() #closing pool
